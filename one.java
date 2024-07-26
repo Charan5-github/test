526d27538886d1db2dcfbdfafc558586a86741d4
charan <charan.dhayapule@gmail.com>
2024-07-26 19:17:12 +0530

    gg

---
+++ one.java
@@ -0,0 +1,13 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    // it is massage
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
